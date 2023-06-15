d47323343af035df96731fbe85320e77e53d9879
matheus.fleonardi <matheus.fleonardi@SBC0601501W10-1>
2023-06-15 08:38:53 -0300

    Adicionar arquivos de projeto.

---
+++ TrocoMoedas/FormTroco.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace TrocoMoedas
+{
+    partial class FormTroco12
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_valorcompra = new System.Windows.Forms.Label();
+            this.lbl_valorpago = new System.Windows.Forms.Label();
+            this.lbl_valortroco = new System.Windows.Forms.Label();
+            this.lbl_troco100 = new System.Windows.Forms.Label();
+            this.lbl_troco050 = new System.Windows.Forms.Label();
+            this.lbl_troco025 = new System.Windows.Forms.Label();
+            this.lbl_troco010 = new System.Windows.Forms.Label();
+            this.lbl_troco05 = new System.Windows.Forms.Label();
+            this.lbl_troco01 = new System.Windows.Forms.Label();
+            this.lbl_troco1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbl_troco50 = new System.Windows.Forms.Label();
+            this.lbl_troco25 = new System.Windows.Forms.Label();
+            this.lbl_troco10 = new System.Windows.Forms.Label();
+            this.lbl_troco005 = new System.Windows.Forms.Label();
+            this.txtbox_Valorcompra = new System.Windows.Forms.TextBox();
+            this.txtbox_valorpago = new System.Windows.Forms.TextBox();
+            this.txtbox_Valortroco = new System.Windows.Forms.TextBox();
+            this.lbl_calcular = new System.Windows.Forms.Button();
+            this.lbl_troco001 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbox_valorcartao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite os valores abaixo";
+            // 
+            // lbl_valorcompra
+            // 
+            this.lbl_valorcompra.AutoSize = true;
+            this.lbl_valorcompra.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_valorcompra.Location = new System.Drawing.Point(16, 113);
+            this.lbl_valorcompra.Name = "lbl_valorcompra";
+            this.lbl_valorcompra.Size = new System.Drawing.Size(143, 21);
+            this.lbl_valorcompra.TabIndex = 1;
+            this.lbl_valorcompra.Text = "Valor de Compra";
+            // 
+            // lbl_valorpago
+            // 
+            this.lbl_valorpago.AutoSize = true;
+            this.lbl_valorpago.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_valorpago.Location = new System.Drawing.Point(12, 165);
+            this.lbl_valorpago.Name = "lbl_valorpago";
+            this.lbl_valorpago.Size = new System.Drawing.Size(198, 21);
+            this.lbl_valorpago.TabIndex = 2;
+            this.lbl_valorpago.Text = "Valor pago em Dinheiro";
+            // 
+            // lbl_valortroco
+            // 
+            this.lbl_valortroco.AutoSize = true;
+            this.lbl_valortroco.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_valortroco.Location = new System.Drawing.Point(12, 285);
+            this.lbl_valortroco.Name = "lbl_valortroco";
+            this.lbl_valortroco.Size = new System.Drawing.Size(126, 21);
+            this.lbl_valortroco.TabIndex = 3;
+            this.lbl_valortroco.Text = "Valor do Troco";
+            // 
+            // lbl_troco100
+            // 
+            this.lbl_troco100.AutoSize = true;
+            this.lbl_troco100.Location = new System.Drawing.Point(505, 84);
+            this.lbl_troco100.Name = "lbl_troco100";
+            this.lbl_troco100.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco100.TabIndex = 4;
+            this.lbl_troco100.Text = "0";
+            this.lbl_troco100.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco050
+            // 
+            this.lbl_troco050.AutoSize = true;
+            this.lbl_troco050.Location = new System.Drawing.Point(505, 123);
+            this.lbl_troco050.Name = "lbl_troco050";
+            this.lbl_troco050.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco050.TabIndex = 5;
+            this.lbl_troco050.Text = "0";
+            this.lbl_troco050.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco025
+            // 
+            this.lbl_troco025.AutoSize = true;
+            this.lbl_troco025.Location = new System.Drawing.Point(505, 161);
+            this.lbl_troco025.Name = "lbl_troco025";
+            this.lbl_troco025.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco025.TabIndex = 6;
+            this.lbl_troco025.Text = "0";
+            this.lbl_troco025.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco010
+            // 
+            this.lbl_troco010.AutoSize = true;
+            this.lbl_troco010.Location = new System.Drawing.Point(505, 200);
+            this.lbl_troco010.Name = "lbl_troco010";
+            this.lbl_troco010.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco010.TabIndex = 7;
+            this.lbl_troco010.Text = "0";
+            this.lbl_troco010.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco05
+            // 
+            this.lbl_troco05.AutoSize = true;
+            this.lbl_troco05.Location = new System.Drawing.Point(505, 244);
+            this.lbl_troco05.Name = "lbl_troco05";
+            this.lbl_troco05.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco05.TabIndex = 8;
+            this.lbl_troco05.Text = "0";
+            this.lbl_troco05.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco01
+            // 
+            this.lbl_troco01.AutoSize = true;
+            this.lbl_troco01.Location = new System.Drawing.Point(505, 291);
+            this.lbl_troco01.Name = "lbl_troco01";
+            this.lbl_troco01.Size = new System.Drawing.Size(13, 15);
+            this.lbl_troco01.TabIndex = 9;
+            this.lbl_troco01.Text = "0";
+            this.lbl_troco01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_troco1
+            // 
+            this.lbl_troco1.AutoSize = true;
+            this.lbl_troco1.Font = new System.Drawing.Font("Swis721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco1.Location = new System.Drawing.Point(544, 80);
+            this.lbl_troco1.Name = "lbl_troco1";
+            this.lbl_troco1.Size = new System.Drawing.Size(156, 19);
+            this.lbl_troco1.TabIndex = 11;
+            this.lbl_troco1.Text = "Moedas de R$ 1,00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Square721 BT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(544, 110);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 15);
+            this.label13.TabIndex = 12;
+            // 
+            // lbl_troco50
+            // 
+            this.lbl_troco50.AutoSize = true;
+            this.lbl_troco50.Font = new System.Drawing.Font("Square721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco50.Location = new System.Drawing.Point(544, 119);
+            this.lbl_troco50.Name = "lbl_troco50";
+            this.lbl_troco50.Size = new System.Drawing.Size(167, 20);
+            this.lbl_troco50.TabIndex = 13;
+            this.lbl_troco50.Text = "Moedas de R$ 0,50";
+            // 
+            // lbl_troco25
+            // 
+            this.lbl_troco25.AutoSize = true;
+            this.lbl_troco25.Font = new System.Drawing.Font("Square721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco25.Location = new System.Drawing.Point(544, 157);
+            this.lbl_troco25.Name = "lbl_troco25";
+            this.lbl_troco25.Size = new System.Drawing.Size(167, 20);
+            this.lbl_troco25.TabIndex = 14;
+            this.lbl_troco25.Text = "Moedas de R$ 0,25";
+            // 
+            // lbl_troco10
+            // 
+            this.lbl_troco10.AutoSize = true;
+            this.lbl_troco10.Font = new System.Drawing.Font("Square721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco10.Location = new System.Drawing.Point(570, 196);
+            this.lbl_troco10.Name = "lbl_troco10";
+            this.lbl_troco10.Size = new System.Drawing.Size(141, 20);
+            this.lbl_troco10.TabIndex = 15;
+            this.lbl_troco10.Text = "moedas R$ 0,10";
+            // 
+            // lbl_troco005
+            // 
+            this.lbl_troco005.AutoSize = true;
+            this.lbl_troco005.Font = new System.Drawing.Font("Square721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco005.Location = new System.Drawing.Point(544, 240);
+            this.lbl_troco005.Name = "lbl_troco005";
+            this.lbl_troco005.Size = new System.Drawing.Size(167, 20);
+            this.lbl_troco005.TabIndex = 16;
+            this.lbl_troco005.Text = "Moedas de R$ 0,05";
+            // 
+            // txtbox_Valorcompra
+            // 
+            this.txtbox_Valorcompra.Location = new System.Drawing.Point(216, 113);
+            this.txtbox_Valorcompra.Name = "txtbox_Valorcompra";
+            this.txtbox_Valorcompra.Size = new System.Drawing.Size(200, 23);
+            this.txtbox_Valorcompra.TabIndex = 17;
+            // 
+            // txtbox_valorpago
+            // 
+            this.txtbox_valorpago.Location = new System.Drawing.Point(216, 167);
+            this.txtbox_valorpago.Name = "txtbox_valorpago";
+            this.txtbox_valorpago.Size = new System.Drawing.Size(200, 23);
+            this.txtbox_valorpago.TabIndex = 18;
+            // 
+            // txtbox_Valortroco
+            // 
+            this.txtbox_Valortroco.Location = new System.Drawing.Point(222, 283);
+            this.txtbox_Valortroco.Name = "txtbox_Valortroco";
+            this.txtbox_Valortroco.Size = new System.Drawing.Size(200, 23);
+            this.txtbox_Valortroco.TabIndex = 19;
+            // 
+            // lbl_calcular
+            // 
+            this.lbl_calcular.AutoSize = true;
+            this.lbl_calcular.Location = new System.Drawing.Point(100, 363);
+            this.lbl_calcular.Name = "lbl_calcular";
+            this.lbl_calcular.Size = new System.Drawing.Size(222, 75);
+            this.lbl_calcular.TabIndex = 20;
+            this.lbl_calcular.Text = "Calcular";
+            this.lbl_calcular.UseVisualStyleBackColor = true;
+            // 
+            // lbl_troco001
+            // 
+            this.lbl_troco001.AutoSize = true;
+            this.lbl_troco001.Font = new System.Drawing.Font("Square721 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_troco001.Location = new System.Drawing.Point(544, 285);
+            this.lbl_troco001.Name = "lbl_troco001";
+            this.lbl_troco001.Size = new System.Drawing.Size(167, 20);
+            this.lbl_troco001.TabIndex = 21;
+            this.lbl_troco001.Text = "Moedas de R$ 0,01";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(493, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 25);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Moedas para troco";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 21);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Valor pago no Cartão";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtbox_valorcartao
+            // 
+            this.txtbox_valorcartao.Location = new System.Drawing.Point(216, 224);
+            this.txtbox_valorcartao.Name = "txtbox_valorcartao";
+            this.txtbox_valorcartao.Size = new System.Drawing.Size(206, 23);
+            this.txtbox_valorcartao.TabIndex = 24;
+            this.txtbox_valorcartao.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FormTroco12
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbox_valorcartao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_troco001);
+            this.Controls.Add(this.lbl_calcular);
+            this.Controls.Add(this.txtbox_Valortroco);
+            this.Controls.Add(this.txtbox_valorpago);
+            this.Controls.Add(this.txtbox_Valorcompra);
+            this.Controls.Add(this.lbl_troco005);
+            this.Controls.Add(this.lbl_troco10);
+            this.Controls.Add(this.lbl_troco25);
+            this.Controls.Add(this.lbl_troco50);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lbl_troco1);
+            this.Controls.Add(this.lbl_troco01);
+            this.Controls.Add(this.lbl_troco05);
+            this.Controls.Add(this.lbl_troco010);
+            this.Controls.Add(this.lbl_troco025);
+            this.Controls.Add(this.lbl_troco050);
+            this.Controls.Add(this.lbl_troco100);
+            this.Controls.Add(this.lbl_valortroco);
+            this.Controls.Add(this.lbl_valorpago);
+            this.Controls.Add(this.lbl_valorcompra);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FormTroco12";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Troco em moedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lbl_valorcompra;
+        private Label lbl_valorpago;
+        private Label lbl_valortroco;
+        private Label lbl_troco100;
+        private Label lbl_troco050;
+        private Label lbl_troco025;
+        private Label lbl_troco010;
+        private Label lbl_troco05;
+        private Label lbl_troco01;
+        private Label lbl_troco1;
+        private Label label13;
+        private Label lbl_troco50;
+        private Label lbl_troco25;
+        private Label lbl_troco10;
+        private Label lbl_troco005;
+        private TextBox txtbox_Valorcompra;
+        private TextBox txtbox_valorpago;
+        private TextBox txtbox_Valortroco;
+        private Button lbl_calcular;
+        private Label lbl_troco001;
+        private Label label3;
+        private Label label4;
+        private TextBox txtbox_valorcartao;
+    }
+}
---
+++ TrocoMoedas/FormTroco.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocoMoedas
+{
+    public partial class FormTroco12 : Form
+    {
+        public FormTroco12()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_calcular_Click(object sender, EventArgs e)
+        {
+            decimal valorCompra;
+            decimal valorPagoDinheiro;
+            decimal valorTroco;
+
+            valorCompra = Convert.ToDecimal(txtbox_Valorcompra.Text);
+            valorPagoDinheiro = Convert.ToDecimal(txtbox_valorpago.Text);
+
+            valorTroco = valorPagoDinheiro - valorCompra;
+
+            txtbox_Valortroco.Text = valorTroco.ToString();
+
+            int moedas1 = (int)(valorTroco / 1m);
+            lbl_troco1.Text = moedas1.ToString();
+            valorTroco = valorTroco % 1;
+
+            int moedas050 = (int)(valorTroco / 0.050m);
+            lbl_troco50.Text = moedas050.ToString();
+            valorCompra = valorTroco % 0.50m;
+
+            int moedas025 = (int)(valorTroco / 0.025m);
+            lbl_troco25.Text = moedas025.ToString();
+            valorTroco = valorTroco % 0.25m;
+
+            int moedas10 = (int)(valorTroco * 0.10m);
+            lbl_troco10.Text = moedas10.ToString();
+            valorTroco = valorTroco % 0.10m;
+
+            int moedas005 = (int)(valorTroco * 0.05m);
+            lbl_troco005.Text = moedas005.ToString();
+            valorTroco = valorTroco % 0.05m;
+
+            int moedas001 = (int)(valorTroco * 0.01m);
+            lbl_troco001.Text = moedas001.ToString();
+            valorTroco = valorTroco % 0.1m;
+
+        }
+    }
+}
